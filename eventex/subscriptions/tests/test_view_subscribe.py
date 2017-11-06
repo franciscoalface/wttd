ecbf27e77d96bc2dd5c5d62a000ef03db5287444
franciscoalface <falface@gmail.com>
2017-11-05 22:53:50 -0200

    Config travis e landscape

--- eventex/subscriptions/tests/test_view_subscribe.py
+++ eventex/subscriptions/tests/test_view_subscribe.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 from eventex.subscriptions.forms import SubscriptionForm
 
 
-class SubscribeTest(TestCase):
+class SubscribeGet(TestCase):
     def setUp(self):
         self.response = self.client.get('/inscricao/')
 
@@ -17,32 +17,30 @@ class SubscribeTest(TestCase):
 
     def test_html(self):
         """HTML must contain imput tags"""
-        self.assertContains(self.response, '<form')
-        self.assertContains(self.response, '<input', 6)
-        self.assertContains(self.response, 'type="text"', 3)
-        self.assertContains(self.response, 'type="email"')
-        self.assertContains(self.response, 'type="submit"')
+        tags = (('<form', 1),
+                ('<input', 6),
+                ('type="text"', 3),
+                ('type="email"', 1),
+                ('type="submit"', 1))
+
+        for text, count in tags:
+            with self.subTest():
+                self.assertContains(self.response, text, count)
 
     def test_csrf(self):
         """HTML must contain CSRF"""
         self.assertContains(self.response, 'csrfmiddlewaretoken')
 
     def test_has_form(self):
-        """COntext must have subscription form"""
+        """Context must have subscription form"""
         form = self.response.context['form']
         self.assertIsInstance(form, SubscriptionForm)
 
-    def test_form_has_fields(self):
-        """Form must have 4 fields"""
-        form = self.response.context['form']
-        self.assertSequenceEqual(['name', 'cpf', 'email', 'phone'], list(form.fields))
 
-
-class SubscribePostTest(TestCase):
+class SubscribePostValid(TestCase):
     def setUp(self):
         data = dict(name='Henrique Bastos', cpf='12345678901', email='henrique@bastos.net', phone='21-99618-6180')
         self.response = self.client.post('/inscricao/', data)
-        self.email = mail.outbox[0]
 
     def test_post(self):
         """Valid POST should redirect to /inscricao/"""
@@ -51,26 +49,8 @@ class SubscribePostTest(TestCase):
     def test_send_subscribe_email(self):
         self.assertEqual(1, len(mail.outbox))
 
-    def test_subscription_email_subject(self):
-        expect = 'Confirmação de inscrição'
-        self.assertEqual(expect, self.email.subject)
 
-    def test_subscription_email_from(self):
-        expect = 'contato@eventex.com.br'
-        self.assertEqual(expect, self.email.from_email)
-
-    def teste_subscription_email_to(self):
-        expect = ['contato@eventex.com.br', 'henrique@bastos.net']
-        self.assertEqual(expect, self.email.to)
-
-    def teste_subscription_email_body(self):
-        self.assertIn('Henrique Bastos', self.email.body)
-        self.assertIn('12345678901', self.email.body)
-        self.assertIn('henrique@bastos.net', self.email.body)
-        self.assertIn('21-99618-6180', self.email.body)
-
-
-class SubscribeInvalidPost(TestCase):
+class SubscribePostInvalid(TestCase):
     def setUp(self):
         self.response = self.client.post('/inscricao/', {})
         self.form = self.response.context['form']
@@ -87,6 +67,7 @@ class SubscribeInvalidPost(TestCase):
 
     def test_form_has_errors(self):
         self.assertTrue(self.form.errors)
+
 
 class SubscribeSuccessMessage(TestCase):
     def test_message(self):
