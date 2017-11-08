bf5c0dc68a8d6c5b7de42b3ca7aa5cb7459504c9
franciscoalface <falface@gmail.com>
2017-11-08 20:55:41 -0200

    Persiste inscrição no BD

---
+++ eventex/subscriptions/tests/test_model_subscription.py
@@ -0,0 +1,21 @@
+from datetime import datetime
+from django.test import TestCase
+from eventex.subscriptions.models import Subscription
+
+
+class SubscriptionModelTest(TestCase):
+    def setUp(self):
+        self.obj = Subscription(
+            name='Henrique Bastos',
+            cpf='12345678901',
+            email='henrique@bastos.net',
+            phone='21-996186180'
+        )
+        self.obj.save()
+
+    def test_create(self):
+        self.assertTrue(Subscription.objects.exists())
+
+    def test_created_at(self):
+        """Subscription must have an auto created at attr"""
+        self.assertIsInstance(self.obj.created_at, datetime)
--- eventex/subscriptions/views.py
+++ eventex/subscriptions/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 from django.template.loader import render_to_string
 from eventex.subscriptions.forms import SubscriptionForm
 from django.conf import settings
+from eventex.subscriptions.models import Subscription
 
 
 def subscribe(request):
@@ -25,6 +26,8 @@ def create(request):
                form.cleaned_data['email'],
                'subscriptions/subscription_email.txt',
                form.cleaned_data)
+
+    Subscription.objects.create(**form.cleaned_data)
 
     messages.success(request, 'Inscrição realizada com sucesso!')
 
